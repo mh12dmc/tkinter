ef1d83ea167529ada0f0ed887e7d693b0a15e4e7
mhrMugiwara <mohammad.mhrmhr12@gmail.com>
2024-11-01 00:04:16 +0330

    create display

--- mh12_calculator/main.py
+++ mh12_calculator/main.py
@@ -1,11 +1,27 @@
 from tkinter import *
 
+def display_config():
+    display_text.set('mhr')
+
 
 
 if __name__ == "__main__":
     
     root = Tk()
+    root.title("mhr_calculator")
     
+
+    display_frame = Frame(root)
+    display_frame.grid()
+
+    display_text = StringVar()
+    display_lbl = Label(display_frame, textvariable=display_text, height=1, width=20, relief='sunken',
+                        borderwidth=10, bg='#D8D2C2', font=('FreeSerif', 50))
+    display_lbl.pack()
+####todo
+    btn1 = Button(root, text='1', command=display_config)
+    btn1.grid(row=1, column=0)
+
 
 
     root.mainloop()
